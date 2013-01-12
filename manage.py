0148cc3e6351d3fa26616e6d3f3a3510c490f172
saipanno <kid.xiyang@gmail.com>
2013-01-12 23:51:54 +0800

    删除flask-script插件

--- manage.py
+++ manage.py
@@ -6,14 +6,9 @@
 #
 #    Created at 2013/01/12. Ruoyan Wong(@saipanno).
 
-from flask.ext.script import Manager
-
 from application import app
 app.config.from_object('config')
 
-manager = Manager(app)
-manager.add_option("-c", "--config", dest="config", required=False)
-
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
