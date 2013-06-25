a12c45f7debe34d40c498284f3c5392567a6ce84
saipanno <kid.xiyang@gmail.com>
2013-06-25 18:42:26 +0800

    增加自定义脚本选择验证

--- web/forms/operate.py
+++ web/forms/operate.py
@@ -47,50 +47,43 @@ class CreatePingDetectForm(Form):
 
 class CreatePreDefinedExecuteForm(Form):
 
-    vars_desc = u'''<p>用<code>|</code> 作为key(域名或IP地址)和value的分隔符</p>
-<p>用<code>,</code> 作为多个变量赋值的分隔符</p>
-<p>用<code>=</code> 作为变量赋值的分隔符</p>
-<strong>例如:</strong>
-<p><code>60.175.193.194|address=61.132.226.195,gateway=61.132.226.254</code></p>'''
+    server_desc = u'支持域名或IP地址,一行一个.'
+
+    script_desc = u'较为常用的预定义脚本.'
+
+    vars_desc = u'''<p>用 <code>|</code> 作为key(IP地址)和value的分隔符, 用 <code>,</code> 作为value(多个变量赋值)的分隔符, \
+    用 <code>=</code> 作为单变量的赋值符。 如:</p>
+<code>60.175.193.194|address=61.132.226.195,gateway=61.132.226.254</code>'''
+
+    config_desc = u'SSH配置.包含SSH端口,用户名,密码以及密钥(可选).'
 
     next_page = HiddenField()
-    server_list = TextAreaField(u'服务器列表:', id='textarea', description=u'支持域名或IP地址,一行一个.', default=u'')
-    template_script = QuerySelectField(u'预定义脚本:', id='select', description=u'较为常用的预定义脚本.',
+    server_list = TextAreaField(u'服务器列表:', id='textarea', description=server_desc, default=u'')
+    template_script = QuerySelectField(u'预定义脚本:', id='select', description=script_desc,
                                        query_factory=PreDefinedScript.query.all,  get_label='desc')
-    template_vars = TextAreaField(u'模板变量:', id='textarea', description=vars_desc, default=u'')
-    ssh_config = QuerySelectField(u'SSH配置', id='select', description=u'SSH配置.包含SSH端口,用户名,密码以及密钥(可选).',
-                                  query_factory=SshConfig.query.all,  get_label='desc')
+    template_vars = TextAreaField(u'自定义变量:', id='textarea', description=vars_desc, default=u'')
+    ssh_config = QuerySelectField(u'SSH配置', id='select', description=config_desc, query_factory=SshConfig.query.all,
+                                  get_label='desc')
     submit = SubmitField(u'Continue', id='submit')
 
 
 class CreateCustomExecuteForm(Form):
 
-    script_desc = u'''<p>用<code>{</code>和<code>}</code>作为外部变量的定界符,此类变量会自动按照变量文件中的定义进行替换.</p>
-<p>同时模板依然支持shell中的<code>$</code>变量</p>
-<pre>for device in `/sbin/ifconfig -a | awk '/^e/ { print $1 }'`; do
-    mac=`ifconfig $device | grep HWaddr | awk '{ print $5 }'`
-    if [ "$device" == "eth0" -o "$device" == "em1" ]; then
-        echo "DEVICE=$device"    > /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "TYPE=Ethernet"     >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "BOOTPROTO=static"  >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "HWADDR=$mac"       >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "IPADDR={address}"  >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "NETMASK={netmask}" >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "GATEWAY={gateway}" >> /etc/sysconfig/network-scripts/ifcfg-$device
-        echo "ONBOOT=yes"        >> /etc/sysconfig/network-scripts/ifcfg-$device
-    fi
-done</pre>'''
+    server_desc = u'支持域名或IP地址,一行一个.'
 
-    vars_desc = u'''<p>用<code>|</code> 作为key(域名或IP地址)和value的分隔符</p>
-<p>用<code>,</code> 作为多个变量赋值的分隔符</p>
-<p>用<code>=</code> 作为变量赋值的分隔符</p>
-<strong>例如:</strong>
-<p><code>60.175.193.194|address=61.132.226.195,gateway=61.132.226.254</code></p>'''
+    script_desc = u'''<p>用 <code>{</code> 和 <code>}</code> 作为外部变量的定界符,此类变量会依据变量文件中的定义进行替换, \
+    同时模板依然支持shell中的 <code>$</code> 变量。'''
+
+    vars_desc = u'''<p>用 <code>|</code> 作为key(IP地址)和value的分隔符, 用 <code>,</code> 作为value(多个变量赋值)的分隔符, \
+    用 <code>=</code> 作为单变量的赋值符。 如:</p>
+<code>60.175.193.194|address=61.132.226.195,gateway=61.132.226.254</code>'''
+
+    config_desc = u'SSH配置.包含SSH端口,用户名,密码以及密钥(可选).'
 
     next_page = HiddenField()
-    server_list = TextAreaField(u'服务器列表:', id='textarea', description=u'支持域名或IP地址,一行一个.', default=u'')
+    server_list = TextAreaField(u'服务器列表:', id='textarea', description=server_desc, default=u'')
     template_script = TextAreaField(u'脚本/脚本模板:', id='textarea', description=script_desc, default=u'')
-    template_vars = TextAreaField(u'模板变量:', id='textarea', description=vars_desc, default=u'')
-    ssh_config = QuerySelectField(u'SSH配置', id='select', description=u'SSH配置.包含SSH端口,用户名,密码以及密钥(可选).',
-                                  query_factory=SshConfig.query.all,  get_label='desc')
+    template_vars = TextAreaField(u'自定义变量:', id='textarea', description=vars_desc, default=u'')
+    ssh_config = QuerySelectField(u'SSH配置', id='select', description=config_desc, query_factory=SshConfig.query.all,
+                                  get_label='desc')
     submit = SubmitField(u'Continue', id='submit', description='submit')
