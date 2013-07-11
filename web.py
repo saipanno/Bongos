e557e1b476a96fd7c93812432966cbf74e453971
saipanno <kid.xiyang@gmail.com>
2013-07-11 10:30:37 +0800

    恢复app.run的配置传入功能

--- web.py
+++ web.py
@@ -32,4 +32,4 @@ app = create_app()
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host=app.config['HOST'], port=int(app.config['PORT']), debug=app.config['DEBUG'])
