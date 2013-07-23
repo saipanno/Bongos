f8095b20910df4882fc5a2d828da4d660c3b0d27
saipanno <kid.xiyang@gmail.com>
2013-07-23 23:46:04 +0800

    删除已完成TODO说明

--- frontend/models/account.py
+++ frontend/models/account.py
@@ -33,8 +33,6 @@ class User(db.Model):
 
     __tablename__ = 'user_lists'
 
-    # TODO: 增加用户的多组支持
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(50), unique=True)
